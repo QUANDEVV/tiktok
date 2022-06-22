edbe3a79dbf2a0f3f134554eb417bc4bf9712fa6
web3Codess <vercelploy@gmail.com>
2022-06-22 10:40:08 +0300

    commit on 'origin'

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import "./VideoSidebar.css";
-// import FavoriteIcon from "@material-ui/icons/Favorite";
-// import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-// import MessageIcon from "@material-ui/icons/Message";
-// import ShareIcon from "@material-ui/icons/Share";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import MessageIcon from '@mui/icons-material/Message';
+import ShareIcon from '@mui/icons-material/Share';
 
 function VideoSidebar({ likes, shares, messages }) {
-  // const [liked, setLiked] = useState(false);
+  const [liked, setLiked] = useState(false);
 
   return (
     <div className="videoSidebar">
       <div className="videoSidebar__button">
-        {/* {liked ? ( */}
-          {/* <FavoriteIcon fontSize="large" onClick={(e) => setLiked(false)} /> */}
+        {liked ? (
+          <FavoriteIcon fontSize="large" onClick={(e) => setLiked(false)} />
         ) : (
-          {/* <FavoriteBorderIcon
+          <FavoriteBorderIcon
             fontSize="large"
             onClick={(e) => setLiked(true)}
-          /> */}
+          />
         )}
-        {/* <p>{liked ? likes + 1 : likes}</p> */}
+        <p>{liked ? likes + 1 : likes}</p>
       </div>
       <div className="videoSidebar__button">
-        {/* <MessageIcon fontSize="large" /> */}
+        <MessageIcon fontSize="large" />
         <p>{messages}</p>
       </div>
       <div className="videoSidebar__button">
-        {/* <ShareIcon fontSize="large" /> */}
+        <ShareIcon fontSize="large" />
         <p>{shares}</p>
       </div>
     </div>
